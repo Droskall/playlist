0e21ff4080166cd2b317a058471b0ad235f87990
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2021-12-21 18:54:34 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let containerMusics = $("#musics");
+let containerSongs = $("#containerSongs").first()
+let containerVideos = $("#video");
+let index = 0;
+let nbClick = 0;
+
+$("#createPlaylist").click(function () {
+    $("#containerCreatePlaylist").css("display", "flex");
+})
+
